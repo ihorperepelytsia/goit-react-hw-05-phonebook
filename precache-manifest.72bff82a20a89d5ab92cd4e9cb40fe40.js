4859cb082447c1e1e32b83fd4d8fb488fa99cfa3
Ihor Perepelytsia <igorekperepelitsa@gmail.com>
2020-06-08 11:28:46 +0300

    Updates

---
+++ precache-manifest.72bff82a20a89d5ab92cd4e9cb40fe40.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa47c68cb9c7b57a0977c2e099d2fe4b",
+    "url": "/goit-react-hw-05-phonebook/index.html"
+  },
+  {
+    "revision": "26fef4d667b0f2ffc796",
+    "url": "/goit-react-hw-05-phonebook/static/css/main.c28d5ef0.chunk.css"
+  },
+  {
+    "revision": "5cd44948ced5838ec968",
+    "url": "/goit-react-hw-05-phonebook/static/js/2.dfbac529.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-05-phonebook/static/js/2.dfbac529.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26fef4d667b0f2ffc796",
+    "url": "/goit-react-hw-05-phonebook/static/js/main.4b239b3c.chunk.js"
+  },
+  {
+    "revision": "d7775061231ef60b40f4",
+    "url": "/goit-react-hw-05-phonebook/static/js/runtime-main.8bbef633.js"
+  }
+]);
